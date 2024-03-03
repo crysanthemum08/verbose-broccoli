751a98a7b6725d0f82058dcba5267b49b65729f6
JlnnTrrs <115263318+JlnnTrrs@users.noreply.github.com>
2024-03-03 02:33:51 -0800

    cookie_consent.php

--- verbose-broccoli/cookie_consent.php
+++ verbose-broccoli/cookie_consent.php
@@ -19,7 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <style>
         .cookies{
             font-size: 15px;
